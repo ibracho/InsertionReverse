b5e36b7c24576f2fad3dd4c52e55ed1f74182a4a
ibracho <isabelgbracho@outlook.com>
2024-09-12 23:24:26 -0500

    done

---
+++ Insertion/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Pair
+{
+    public int Key;
+    public string Value;
+
+    public Pair(int key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"({Key}, \"{Value}\")";
+    }
+}
+
+public class Solution
+{
+    public List<List<Pair>> InsertionSort(List<Pair> pairs)
+    {
+        int n = pairs.Count;
+        List<List<Pair>> res = new List<List<Pair>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int j = i - 1;
+
+
+            while (j >= 0 && pairs[j].Key > pairs[j + 1].Key)
+            {
+                Pair temp = pairs[j];
+                pairs[j] = pairs[j + 1];
+                pairs[j + 1] = temp;
+                j--;
+            }
+
+
+            List<Pair> cloneList = new List<Pair>(pairs);
+            res.Add(cloneList);
+        }
+        return res;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Pair> pairs1 = new List<Pair>
+        {
+            new Pair(5, "apple"),
+            new Pair(2, "banana"),
+            new Pair(9, "cherry")
+        };
+
+        List<Pair> pairs2 = new List<Pair>
+        {
+            new Pair(3, "cat"),
+            new Pair(3, "bird"),
+            new Pair(2, "dog")
+        };
+
+        Solution solution = new Solution();
+
+        List<List<Pair>> result1 = solution.InsertionSort(pairs1);
+        List<List<Pair>> result2 = solution.InsertionSort(pairs2);
+
+        Console.WriteLine("Example 1:");
+        foreach (var state in result1)
+        {
+            Console.WriteLine(string.Join(", ", state));
+        }
+
+        Console.WriteLine("\nExample 2:");
+        foreach (var state in result2)
+        {
+            Console.WriteLine(string.Join(", ", state));
+        }
+    }
+}
---
+++ Reverse/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public class ListNode
+{
+    public int val;
+    public ListNode? next;
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode? ReverseList(ListNode? head)
+    {
+        ListNode? cur = head;
+        ListNode? prev = null;
+        ListNode? nxt = null;
+        while (cur != null)
+        {
+            nxt = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+
+        return prev;
+    }
+
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        // Example 1
+        ListNode head1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode? reversedHead1 = solution.ReverseList(head1);
+        PrintList(reversedHead1);
+
+        // Example 2
+        ListNode head2 = new ListNode(1, new ListNode(2));
+        ListNode? reversedHead2 = solution.ReverseList(head2);
+        PrintList(reversedHead2);
+
+        // Example 3
+        ListNode? head3 = null;
+        ListNode? reversedHead3 = solution.ReverseList(head3);
+        PrintList(reversedHead3);
+    }
+
+    public static void PrintList(ListNode? head)
+    {
+        if (head == null)
+        {
+            Console.WriteLine("[]");
+            return;
+        }
+
+        Console.Write("[");
+        ListNode? current = head;
+        while (current != null)
+        {
+            Console.Write(current.val);
+            current = current.next;
+            if (current != null)
+            {
+                Console.Write(",");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
